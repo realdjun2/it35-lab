dd42bcc376f641193ebad56f33e35a8b64cdc80c
realdjun2 <realdjunodon@gmail.com>
2025-02-14 11:41:13 +0800

    added Menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -8,7 +8,7 @@ import {
       IonToolbar 
   } from '@ionic/react';
   
-  const Template: React.FC = () => {
+  const Menu: React.FC = () => {
     return (
       <IonPage>
         <IonHeader>
@@ -16,7 +16,7 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Template</IonTitle>
+            <IonTitle>Menu</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
@@ -25,4 +25,4 @@ import {
     );
   };
   
-  export default Template;
+  export default Menu;
