c36bc365d7a1a465b8af62cfeae90014e7f80fa3
realdjun2 <realdjunodon@gmail.com>
2025-02-25 16:31:17 +0800

    added style

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -20,6 +20,16 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%',
+            }}
+          >
+            Feed
+          </div>
         </IonContent>
       </IonPage>
     );
