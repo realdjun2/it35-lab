6b232057a7d745a377652422152895a25c70a57c
realdjun2 <realdjunodon@gmail.com>
2025-03-07 17:50:26 +0800

    Updates

--- assets/swipe-back-legacy-CP4YET-J.js
+++ assets/swipe-back-legacy-CP4YET-J.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BccCCItp.js","./index3-legacy-D0Nki3m4.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-B37RU9Ye.js","./index3-legacy-CKidVM0c.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
