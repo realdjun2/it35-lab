f01b1d0b8eaac67345cf01bb96a31c59b5e1f179
realdjun2 <realdjunodon@gmail.com>
2025-03-28 16:34:26 +0800

    Updates

--- assets/md.transition-legacy-DIU1Xnu-.js
+++ assets/md.transition-legacy-DIU1Xnu-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bu-k1apV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BpIU4ncT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
