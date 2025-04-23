79685636db1ec75afa8e5d454f67948368718459
realdjun2 <realdjunodon@gmail.com>
2025-04-23 22:51:20 +0800

    added Editprofilepage

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -17,13 +17,14 @@ import {
       IonToolbar, 
       useIonRouter
   } from '@ionic/react'
-  import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
+  import {homeOutline, logOutOutline, rocketOutline, settingsOutline} from 'ionicons/icons';
 import { Redirect, Route } from 'react-router';
 import Home from './Home';
 import About from './About';
 import Details from './Details';
 import { supabase } from '../utils/supabaseClient';
 import { useState } from 'react';
+import EditProfilePage from './EditProfilePage';
 
 
   const Menu: React.FC = () => {
@@ -35,6 +36,7 @@ import { useState } from 'react';
     const path = [
         {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
         {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
+        {name:'Profile', url: '/it35-lab/app/profile', icon: settingsOutline},
     ]
     const handleLogout = async () => {
         const { error } = await supabase.auth.signOut();
@@ -83,6 +85,7 @@ import { useState } from 'react';
                     <Route exact path="/it35-lab/app/home" component={Home} />
                     <Route exact path="/it35-lab/app/home/details" component={Details} />
                     <Route exact path="/it35-lab/app/about" component={About} />
+                    <Route exact path="/it35-lab/app/profile" component={EditProfilePage} />
 
                     <Route exact path="/it35-lab/app">
                         <Redirect to="/it35-lab/app/home"/>
